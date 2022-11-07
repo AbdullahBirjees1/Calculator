368e6e37f55a12c78290155988080cf54d1777d0
Rana Abdullah <ranaabdullah2341@gmail.com>
2022-11-07 16:45:38 +0500

    Modification in  calculator

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
+var historyData = [];
+var expressionData = "";
+var resultData = "";
 
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+function disply(val) {
+
+  document.getElementById("display-input").value += val;
+  expressionData = val;
+}
+function solve() {
+  let x = document.getElementById("display-input").value;
+  expressionData = x;
+  // var splitUp = x.match(/[^\d()]+|[\d.]+/g);
+  // console.log(x)
+  let y = eval(x);
+  document.getElementById("display-input").value = y;
+  resultData = y;
+  if (historyData.length < 20) {
+    historyData.push({ expression: expressionData, result: resultData });
+    // console.log('historyData',historyData)
+  } else {
+    null;
+  }
+  showLogData();
+  resultData = "";
+  expressionData = "";
+}
+function clr() {
+  document.getElementById("display-input").value = " ";
+}
+function showLogData() {
+  var log = document.getElementById("historyLog");
+  var string = "";
+  
+  for (var key in historyData)
+    string +=
+      "" +
+      historyData[key]["expression"] +
+      " = " +
+      historyData[key]["result"] +
+      "<br>";
+  log.innerHTML = string;
+}
+function clearHistory() {
+  historyData = [];
+  document.getElementById("historyLog").innerHTML = " ";
+}
+$("#display-input").keypress(function (e) {
+  let key = e.which;
+  let checkValues = document.getElementById("display-input").value;
+
+  if (key == 13 && checkValues) {
+    // the enter key code
+    solve();
+  }
+});
